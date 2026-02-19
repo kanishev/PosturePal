32d39eed9b2847dd83dc18ab6a67dd9627306db6
kanishev <kanishevnikita@yandex.kz>
2026-02-19 22:21:31 +0300

    feat(tests): setup tests

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -2,22 +2,22 @@
 import tailwindcss from "@tailwindcss/vite";
 
 export default defineNuxtConfig({
-    compatibilityDate: "2025-07-15",
-    devtools: { enabled: true },
-    css: ["~/assets/css/tailwind.css"],
-    vite: {
-        plugins: [tailwindcss()],
-    },
-    modules: ["shadcn-nuxt", "@pinia/nuxt"],
-    shadcn: {
-        /**
-         * Prefix for all the imported component
-         */
-        prefix: "",
-        /**
-         * Directory that the component lives in.
-         * @default "./components/ui"
-         */
-        componentDir: "@/components/ui",
-    },
+  compatibilityDate: "2025-07-15",
+  devtools: { enabled: true },
+  css: ["~/assets/css/tailwind.css"],
+  vite: {
+    plugins: [tailwindcss()],
+  },
+  modules: ["shadcn-nuxt", "@pinia/nuxt", "@nuxt/test-utils/module"],
+  shadcn: {
+    /**
+     * Prefix for all the imported component
+     */
+    prefix: "",
+    /**
+     * Directory that the component lives in.
+     * @default "./components/ui"
+     */
+    componentDir: "@/components/ui",
+  },
 });

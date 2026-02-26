6405a2ddd491e60d3a9f919eca81cd97b8617fbd
kanishev <kanishevnikita@yandex.kz>
2026-02-26 22:20:19 +0300

    feat: folder structure

--- app/pages/home.vue
+++ app/pages/home.vue
@@ -1,18 +1,10 @@
 <template>
   <main>
-    <h1 class="text-[40px] text-center text-gray-500">
-      {{ website.name }}
-    </h1>
-    <p>{{ website.description }}</p>
+    <h1 class="text-[40px] text-center text-gray-500">Hello</h1>
     <Button>Click me</Button>
   </main>
 </template>
 
 <script setup lang="ts">
-import { Button } from "@/components/ui/button";
-import { useTestStore } from "@/stores/test.ts";
-
-const website = useTestStore();
-
-await callOnce(website.fetch);
+import { Button } from "@/shared/components/ui/button";
 </script>

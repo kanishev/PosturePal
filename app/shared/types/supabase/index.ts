08048c3c3881022f8596a018dfcb4678005376f3
kanishev <kanishevnikita@yandex.kz>
2026-03-09 21:47:38 +0300

    feat: generic types for supabase Table

---
+++ app/shared/types/supabase/index.ts
@@ -0,0 +1,7 @@
+import type { Database } from "./database.types";
+
+type Tables = Database["public"]["Tables"];
+
+export type TableRow<T extends keyof Tables> = Tables[T]["Row"];
+export type TableInsert<T extends keyof Tables> = Tables[T]["Insert"];
+export type TableUpdate<T extends keyof Tables> = Tables[T]["Update"];

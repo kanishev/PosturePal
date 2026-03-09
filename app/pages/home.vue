d58e95b51eda61d3929debb77df0cfeeb3113136
kanishev <kanishevnikita@yandex.kz>
2026-03-09 20:27:13 +0300

    chore: isolate supabase client into shared/api

--- app/pages/home.vue
+++ app/pages/home.vue
@@ -4,11 +4,33 @@
       Hello
     </h1>
     <Button>Click me</Button>
+
+    <ul>
+      <li v-for="instrument in instruments" :key="instrument.id">
+        {{ instrument.name }}
+      </li>
+    </ul>
   </main>
 </template>
 
 <script setup lang="ts">
+import { useRuntimeConfig } from "#app";
 import { Button } from "@/shared/components/ui/button";
+import { onMounted, ref } from "vue";
+
+import { createClient } from "@supabase/supabase-js";
+
+const config = useRuntimeConfig();
+const supabase = createClient(config.public.supabaseUrl, config.public.supabasePublishableKey);
+const instruments = ref([]);
+
+async function getInstruments() {
+  const { data } = await supabase.from("instruments").select();
+  instruments.value = data;
+}
+onMounted(() => {
+  getInstruments();
+});
 
 let a = "";
 a = 1;
---
+++ app/shared/api/supabase.ts
@@ -0,0 +1,9 @@
+import { useRuntimeConfig } from "#app";
+import { createClient } from "@supabase/supabase-js";
+
+const config = useRuntimeConfig();
+
+export const supabase = createClient(
+  config.public.supabaseUrl,
+  config.public.supabasePublishableKey,
+);

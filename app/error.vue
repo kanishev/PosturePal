a77eef51afeae2fa65a4488f32049318918ddc9a
kanishev <kanishevnikita@yandex.kz>
2026-03-23 22:46:18 +0300

    feat: Error page

---
+++ app/error.vue
@@ -0,0 +1,26 @@
+<template>
+  <div class="flex min-h-screen flex-col items-center justify-center gap-4">
+    <template v-if="error">
+      <h1 class="text-4xl font-bold">
+        {{ error.statusCode }}
+      </h1>
+      <p class="text-muted-foreground">
+        {{ error.message }}
+      </p>
+    </template>
+    <Button @click="handleError">
+      На главную
+    </Button>
+  </div>
+</template>
+
+<script setup lang="ts">
+import { clearError, useError } from "#app";
+import { Button } from "~/shared/components/ui/button";
+
+const error = useError();
+
+async function handleError() {
+  await clearError({ redirect: "/" });
+}
+</script>
